2548cd5b27af8d1c43f78758621f27522a5418e2
Alif Munim <alif.munim@ryerson.ca>
2020-04-03 15:35:16 -0400

    Get all profiles and get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -111,4 +111,38 @@ router.post(
   }
 );
 
+// @route   Get api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route   Get api/profile/user/user_id
+// @desc    Get profile by user id
+// @access  Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) return res.status(400).json({ msg: "Profile not found." });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err);
+    if (err.path == "user") {
+      return res.status(400).json({ msg: "Profile not found." });
+    }
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
