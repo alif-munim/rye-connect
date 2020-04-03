54a7b14a0e8c5197ebdbe8af71fab296430ec524
Alif Munim <alif.munim@ryerson.ca>
2020-04-03 15:46:05 -0400

    Delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -111,7 +111,7 @@ router.post(
   }
 );
 
-// @route   Get api/profile
+// @route   GET api/profile
 // @desc    Get all profiles
 // @access  Public
 router.get("/", async (req, res) => {
@@ -141,6 +141,23 @@ router.get("/user/:user_id", async (req, res) => {
     if (err.path == "user") {
       return res.status(400).json({ msg: "Profile not found." });
     }
+    res.status(500).send("Server error");
+  }
+});
+
+// @route   DELETE api/profile
+// @desc    delete profile, user, & posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // @todo - remove user posts
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
     res.status(500).send("Server error");
   }
 });
