7092276f777107a77f24fc9db26f80eff19eaa5f
Alif Munim <alif.munim@ryerson.ca>
2020-06-14 13:29:08 -0400

    Delete education, experiences, and profile
    Signed-off-by: Alif Munim <alif.munim@ryerson.ca>

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,6 +8,7 @@ const { check, validationResult } = require("express-validator");
 // Bring in models
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const Post = require("../../models/Post");
 
 // @route   GET api/profile/me
 // @desc    Get current user's profile
@@ -152,7 +153,9 @@ router.get("/user/:user_id", async (req, res) => {
 // @access  Private
 router.delete("/", auth, async (req, res) => {
   try {
-    // @todo - remove user posts
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
+
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
