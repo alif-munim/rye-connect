a56b821aab0333e4e6cc5837df419b807981e8ae
Alif Munim <alif.munim@ryerson.ca>
2020-06-22 18:49:54 -0400

    Add PostItem components to display individual posts and their properties
    Signed-off-by: Alif Munim <alif.munim@ryerson.ca>

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getPosts } from "../../actions/post";
+import PostItem from "./PostItem";
 import Spinner from "../layout/Spinner";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -10,10 +11,19 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
         getPosts();
     }, [getPosts]);
 
-    return (
-        <div>
-            
-        </div>
+    return loading ? <Spinner /> : (
+        <Fragment>
+            <h1 className="large text-primary">Posts</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i> Welcome to the Community
+            </p>
+            {/* Post Form */}
+            <div className="posts">
+                {posts.map(post => (
+                    <PostItem key={post._id} post={post}/>
+                ))}
+            </div>
+        </Fragment>
     )
 }
 
