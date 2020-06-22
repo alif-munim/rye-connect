c66729353bbdca082cca5e561214474aebe062ec
Alif Munim <alif.munim@ryerson.ca>
2020-06-22 19:40:18 -0400

    Create type, action, and reducer for deleting posts and connect to front-end with react-redux
    Signed-off-by: Alif Munim <alif.munim@ryerson.ca>

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,12 +3,13 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { connect } from "react-redux";
-import { addLike, removeLike } from "../../actions/post";
+import { addLike, removeLike, deletePost } from "../../actions/post";
 
 const PostItem = ({ 
   auth, 
   addLike,
   removeLike,
+  deletePost,
   post: { _id, text, name, avatar, user, likes, comments, date}}) => {
     return (
         <div className="post bg-white p-1 my-1">
@@ -44,7 +45,7 @@ const PostItem = ({
                 )}
             </Link>
             {!auth.loading && user === auth.user._id && (
-                <button type="button" className="btn btn-danger"> 
+                <button onClick={e => deletePost(_id)} type="button" className="btn btn-danger"> 
                     <i className="fas fa-times"></i>
                 </button>
             )}
@@ -59,6 +60,7 @@ PostItem.propTypes = {
     auth: PropTypes.object.isRequired,
     addLike: PropTypes.func.isRequired,
     removeLike: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
@@ -67,5 +69,6 @@ const mapStateToProps = state => ({
 
 export default connect(mapStateToProps, { 
   addLike, 
-  removeLike
+  removeLike,
+  deletePost
 })(PostItem);
