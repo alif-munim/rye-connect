36a61a29ea003ac78eccf51c9c0c0f9e24899bb2
Alif Munim <alif.munim@ryerson.ca>
2020-06-22 19:12:36 -0400

    Add like and unlike functionality to posts using react-redux

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { setAlert } from "./alert";
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES
 } from "./types";
 
 // Get posts
@@ -16,7 +17,39 @@ export const getPosts = () => async dispatch => {
     } catch (err) {
         dispatch({
             type: POST_ERROR,
-            payload: { msg: err.response.statusText, status: err.respons.status }
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+}
+
+// Add like
+export const addLike = id => async dispatch => {
+    try {
+        const res = await axios.put(`api/posts/like/${id}`);
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+}
+
+// Remove like
+export const removeLike = id => async dispatch => {
+    try {
+        const res = await axios.put(`api/posts/unlike/${id}`);
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
         });
     }
 }
