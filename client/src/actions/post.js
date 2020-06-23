4681dd05196ebecef1804a11b3901d02ab993e84
Alif Munim <alif.munim@ryerson.ca>
2020-06-22 20:02:46 -0400

    Add form component to create new post along with required types, actions, and reducers to connect to front-end
    Signed-off-by: Alif Munim <alif.munim@ryerson.ca>

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -4,7 +4,8 @@ import {
     GET_POSTS,
     POST_ERROR,
     UPDATE_LIKES,
-    DELETE_POST
+    DELETE_POST,
+    ADD_POST
 } from "./types";
 
 // Get posts
@@ -65,6 +66,31 @@ export const deletePost = id => async dispatch => {
         });
 
         dispatch(setAlert("Post Removed", "success"));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+}
+
+// Add posts
+export const addPost = formData => async dispatch => {
+
+    const config = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    try {
+        const res = await axios.post("api/posts/", formData, config);
+        dispatch({
+            type: ADD_POST,
+            payload: res.data
+        });
+
+        dispatch(setAlert("Post Created", "success"));
     } catch (err) {
         dispatch({
             type: POST_ERROR,
