b02961cb085c404c06d4364295f6b1c389145ef1
Alif Munim <alif.munim@ryerson.ca>
2020-06-28 13:11:50 -0400

    Prepare for deploy
    Signed-off-by: Alif Munim <alif.munim@ryerson.ca>

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -12,10 +12,9 @@ const Landing = ({ isAuthenticated }) => {
     <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
-          <h1 className="x-large">Developer Connector</h1>
+          <h1 className="x-large">Ryerson Connect</h1>
           <p className="lead">
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
+            <strong>Build</strong> your profile, <strong>share</strong> your skills, <strong>showcase</strong> your projects, <br/>and <strong>connect</strong> with other Ryerson students.
           </p>
           <div className="buttons">
             <Link to="/register" className="btn btn-primary">
